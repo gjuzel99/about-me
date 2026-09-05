9c89ade3769ce43b52ca6686608a992738ae85b4
gjorgjigjuzelov <gjuzelovgjorgji@gmail.com>
2026-09-05 11:57:12 +0200

    Update static domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   // Set this to your production URL (no trailing slash)
-  site: 'https://gjuzel99.github.io',
-  base: '/personal-website',
+  site: 'https://gjorgjigjuzelov.dev',
   integrations: [sitemap()],
 });
